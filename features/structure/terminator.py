04e524622ddca5d53164dbc91b09fa84314a3cb4
Jordan Leppert <jordanleppert@gmail.com>
2019-10-06 11:58:40 +0100

    Terminator optimisation - if functional items haven't changed since last time terminator was suppressed, suppress immediately

--- features/structure/terminator.py
+++ features/structure/terminator.py
@@ -32,6 +32,8 @@ class TerminatorFeature(Feature):
 
         calculator.feature_options['structure.terminator'] = cls.default_options()
 
+        calculator.last_failed_terminator = None
+
     def parse_terminator(self, expr, i, items, flags):
         if 'ignore_terminators' in flags and flags['ignore_terminators']:
             return None, None, None
@@ -40,11 +42,16 @@ class TerminatorFeature(Feature):
         symbol = self.feature_options['structure.terminator']['Symbol']
         if next[0:len(symbol)] == symbol:
             backup_vars = self.vars.copy()
+            func_items = copy_items(functional_items(items))
+            func_item_text = ''.join(ii.text for ii in func_items)
+            if func_item_text == self.last_failed_terminator:
+                return None, None, None
             try:
-                self.execute(copy_items(items), {})
+                self.execute(func_items, {})
             except ExecutionException as e:
                 if str(e.message).startswith('Missing right operands for operator '):
                     self.vars = backup_vars
+                    self.last_failed_terminator = func_item_text
                     return None, None, None
             except Exception:
                 pass
