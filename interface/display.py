1912ee46fcf308b0cf5156a8f58e9e062fbf2058
Jordan Leppert <jordanleppert@gmail.com>
2019-10-12 14:46:16 +0100

    Display is now a set of widgets

--- interface/display.py
+++ interface/display.py
@@ -3,34 +3,36 @@
 from modularcalculator.interface.guitools import *
 from modularcalculator.objects.units import *
 
-from PyQt5.QtWidgets import QTextEdit
 from PyQt5.QtGui import QFontDatabase, QGuiApplication
+from PyQt5.QtWidgets import QTextEdit, QWidget, QGridLayout, QLabel, QVBoxLayout
 
 
-class CalculatorDisplay(QTextEdit):
+class CalculatorDisplay(QWidget):
 
     def __init__(self, interface):
         super().__init__()
+        self.layout = QVBoxLayout()
         self.interface = interface
         self.options = {}
-        self.setReadOnly(True)
+        #self.setReadOnly(True)
         self.defaultStyling()
-        self.initStyling()
+        #self.initStyling()
         self.initOutput()
+        self.setLayout(self.layout)
 
     def defaultStyling(self):
-        self.setFont(QFontDatabase.systemFont(QFontDatabase.FixedFont))
+        #self.setFont(QFontDatabase.systemFont(QFontDatabase.FixedFont))
         qpalette = QGuiApplication.palette()
         self.colours = [ qpalette.base().color().name(), qpalette.alternateBase().color().name() ]
-        self.cssHead = "span.question { font-size: 10pt; } span.answer { font-weight: bold; font-size: 14pt; }"
-        self.cssRow = "div.row{0} {{ background-color: {1} }}"
-        self.answerHtml = "<div class='row{0}'><span class='question'>{1}<br/></span><span class='answer'>{2}{3}</span></div>"
+        #self.cssHead = "span.question { font-size: 10pt; } span.answer { font-weight: bold; font-size: 14pt; }"
+        #self.cssRow = "div.row{0} {{ background-color: {1} }}"
+        #self.answerHtml = "<div class='row{0}'><span class='question'>{1}<br/></span><span class='answer'>{2}{3}</span></div>"
 
-    def initStyling(self):
-        self.css = '<style>' + self.cssHead
-        for i, colour in enumerate(self.colours):
-            self.css += self.cssRow.format(i, colour)
-        self.css += '</style>'
+    # def initStyling(self):
+    #     self.css = '<style>' + self.cssHead
+    #     for i, colour in enumerate(self.colours):
+    #         self.css += self.cssRow.format(i, colour)
+    #     self.css += '</style>'
 
     def initOutput(self):
         self.output = []
@@ -38,9 +40,9 @@ class CalculatorDisplay(QTextEdit):
 
     def addAnswer(self, question, answer, unit):
         self.rawOutput.append(((question, answer, unit)))
-        answerline = self.renderAnswer(question, answer, unit, len(self.output))
-        self.output.append(answerline)
-        self.updateOutput()
+        #answerline = self.renderAnswer(question, answer, unit, len(self.output))
+        #self.output.append(answerline)
+        self.refresh()
 
     def renderAnswer(self, question, answer, unit, n):
         question = question.strip()
@@ -58,7 +60,29 @@ class CalculatorDisplay(QTextEdit):
                 unit = ' ' + unit
         else:
             unit = ''
-        return self.answerHtml.format(n % len(self.colours), questionHtml, htmlSafe(answer), htmlSafe(unit))
+        #return self.answerHtml.format(n % len(self.colours), questionHtml, htmlSafe(answer), htmlSafe(unit))
+        questionDiv = "<div background-color: \"{}\">{}</div>".format(self.colours[n % len(self.colours)], questionHtml)
+
+        layout = QGridLayout()
+
+        questionWidget = QTextEdit()
+        questionWidget.setReadOnly(True)
+        questionFont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        questionFont.setPointSize(10)
+        questionWidget.setFont(questionFont)
+        questionWidget.setHtml(questionDiv)
+        layout.addWidget(questionWidget, 0, 0, 1, 1)
+
+        answerWidget = QLabel(str(answer))
+        answerFont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        answerFont.setPointSize(14)
+        answerWidget.setFont(answerFont)
+        layout.addWidget(answerWidget, 1, 0, 1, 1)
+        
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        return widget
 
     def questionHtml(self, expr):
         statements, _, _ = self.interface.calculatormanager.calculator.parse(expr, {})
@@ -66,8 +90,30 @@ class CalculatorDisplay(QTextEdit):
         return html
 
     def updateOutput(self):
-        self.setHtml(self.css + str.join('', self.output))
-        self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+        self.clearLayout(self.layout)
+
+        for widget in self.output:
+            self.layout.addWidget(widget)
+
+        #self.setLayout(layout)
+        #self.setHtml(self.css + str.join('', self.output))
+        #self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+
+    def clearLayout(self, layout):
+        while True:
+            item = layout.takeAt(0)
+            if item is None:
+                break
+            if item.widget() is not None:
+                widget = item.widget()
+                widget.deleteLater()
+            if item.layout() is not None:
+                childLayout = item.layout()
+                self.clearLayout(childLayout)
+            #item.deleteLater()
+            #item.
+            #del item
+            #layout.removeItem(item)
 
     def clear(self):
         self.initOutput()
