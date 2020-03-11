85f8eb61d3d324df086384c256fb6fea0e99dfcf
Jordan Leppert <jordanleppert@gmail.com>
2020-03-11 10:51:16 +0000

    Packaging modularcalculator so it's easily installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="modularcalculator",
+    version="1.0.0",
+    author="Jordan Leppert",
+    author_email="jordanleppert@gmail.com",
+    description="A powerful, modular calculator written in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JordanL2/ModularCalculator",
+    packages=setuptools.find_packages() + setuptools.find_namespace_packages(include=['modularcalculator.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: LGPL-2.1 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
